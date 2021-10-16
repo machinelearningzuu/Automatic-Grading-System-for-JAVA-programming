ef1dabd1196e61e6df517e3b1c3320caa92f4698
1zuu <isurualagiyawanna9717@gmail.com>
2021-10-16 10:24:26 +0530

    visible GPU count

--- rnn_grading_model.py
+++ rnn_grading_model.py
@@ -16,6 +16,12 @@ from word_embedding import CODE2TENSOR
 from resources.set import Experiments
 from variables import *
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("\n Num GPUs Available: {}\n".format(len(physical_devices)))
+if len(physical_devices):
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
 class SiameseNetwork(object):
     def __init__(self, toggle=-1):
         if toggle != -1:
